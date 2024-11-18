207397fe9c6c9054cfe20aa18d6b294044beab61
Ayomiposi1477 <akinseloyinayomiposi@gmail.com>
2024-11-18 12:47:38 +0000

    second commit

---
+++ logbook.js
@@ -0,0 +1,4 @@
+let signupBtn = document.getDocumentById("signupBtn");
+let signinBtn = document.getDocumentById("signinBtn");
+let nameField = document.getDocumentById("nameField");
+let title = document.getDocumentById("title");
